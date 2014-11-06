888d57a91c861f4defdb4b7b5cb1086f5c946fea
dholdaway <d.m.holdaway@googlemail.com>
2014-11-06 09:05:08 +0000

    updated script

--- upgrade_mysql.sh
+++ upgrade_mysql.sh
@@ -1,4 +1,6 @@
-o "thank you for Choosing to update your mysql instance to 5.6. I am now going to get the file for you"
+#/usr/bin/bash
+
+echo "thank you for Choosing to update your mysql instance to 5.6. I am now going to get the file for you"
 if [ ! -f mysql-5.6.19-linux-glibc2.5-x86_64.tar.gz ]; 
 		then
 				wget "https://downloads.mariadb.com/archives/mysql-5.6/mysql-5.6.19-linux-glibc2.5-x86_64.tar.gz" --no-check-certificate
@@ -35,7 +37,7 @@ if [ ! -f mysql-5.6.19-linux-glibc2.5-x86_64.tar.gz ];
 			echo "###############"
 			sudo sed -i 's/"$/:\/usr\/local\/mysql\/bin"/' /etc/environment
 
-			export $PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/mysql/bin"
+			export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/mysql/bin"
 
 			echo ""
 			echo "###############"
@@ -44,6 +46,8 @@ if [ ! -f mysql-5.6.19-linux-glibc2.5-x86_64.tar.gz ];
 
 			sudo sed -i 's/basedir		= \/usr/basedir		=\/usr\/local\/mysql/' /etc/mysql/my.cnf
 			sudo sed -i 's/lc-messages-dir	= \/usr\/share\/mysql/lc-messages-dir = \/usr\/local\/mysql\/share\nlc-messages		=en_GB\n/' /etc/mysql/my.cnf
+			sudo sed -i 's/myisam-recover	/myisam-recover-options	/' /etc/mysql/my.cnf
+			sudo sed -i 's/key-buffer   /key-buffer-size /' /etc/mysql/my.cnf
 
 			echo ""
 			echo "###############"
@@ -59,8 +63,9 @@ if [ ! -f mysql-5.6.19-linux-glibc2.5-x86_64.tar.gz ];
 			echo "# Starting mysql"
 			echo "###############"
 
-
-			sudo bin/mysqld --user=mysql &
+			sudo cp /usr/local/mysql/support-files/mysql.server /etc/init.d/mysql
+			sudo update-rc.d mysql defaults
+			sudo bin/mysqld_safe --user=mysql &
 
 
 			echo ""
@@ -68,12 +73,10 @@ if [ ! -f mysql-5.6.19-linux-glibc2.5-x86_64.tar.gz ];
 			echo "# running upgrade script"
 			echo "###############"
 
-			mysql_upgrade -u root -pcodilink
+			sudo mysql_upgrade -u root -pcodilink
 
 
 			echo ""
 			echo "###############"
 			echo "# You should now have a fully operational 5.6 installation. Enjoy. Lots of love"
 			echo "###############"
-
-
